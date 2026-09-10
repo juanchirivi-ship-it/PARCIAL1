c6752962aaf29f429b20350550ebb935660acdf5
juanchirivi-ship-it <juan.chirivi@cun.edu.co>
2026-09-10 12:34:43 -0500

    Add files via upload

---
+++ VISUAL TABLAS DEL 5/tabla v5.1/src/App.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Ingrese un número entero: ");
+
+            int numero =scanner.nextInt();
+System.out.println("el numero digitado" + numero);
+           
+// for (int i = 1; i <= 10; i++)
+//             {
+//                 Console.WriteLine(numero + " x " + i + " = " + (numero * i));
+//             }
+
+//             Console.ReadKey();
+//         }
+//     }
+// }
+
+//     }
+
+// }
+
+// for (int i = 1; i <= 10; i++)
+// {
+//     Console.WriteLine(numero + " x " + i + " = " + (numero * i));
+
+// i = 1 → 5 x 1  = 5
+// i = 2 → 5 x 2  = 10
+// i = 3 → 5 x 3  = 15
+// ...
+// i = 10 → 5 x 10 = 50
+}
+
+}
